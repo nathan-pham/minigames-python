5a87bc87ad715c563fcd06235b9dbeaccebe3476
Nathan Pham <nathanpham.me@gmail.com>
2021-02-12 22:11:22 -0800

    working map

--- text-rpg/main.py
+++ text-rpg/main.py
@@ -7,15 +7,17 @@ import textwrap
 
 screen_width = 100
 
-# Initialize player
+# initialize player
 class Player:
     def __init__(self):
         self.name = ""
         self.hp = 0
         self.mp = 0
+        self.explored = []
+        self.location = [1, 1]
         self.status_effects = []
 
-user = Player()
+player = Player()
 
 def start_game():
     print("start game")
@@ -29,7 +31,7 @@ def exit_game():
     sys.exit()
 
 def title_screen():
-    print("Welcome to the Text RPG!")
+    print("\nWelcome to the Text RPG!")
     print("        - play -        ")
     print("        - help -        ")
     print("        - exit -        ")
@@ -47,4 +49,60 @@ def title_options():
         option = input("> ").lower()
     options[option]()
 
+title_screen()
 
+# map
+MAP = [
+    ['x', 'x', 'x', 'x', 'x', 'x'],
+    ['x', 'o', 'o', 'o', 'o', 'x'],
+    ['x', 'o', 'o', 'o', 'o', 'x'],
+    ['x', 'o', 'o', 'o', 'o', 'x'],
+    ['x', 'o', 'o', 'o', 'o', 'x'],
+    ['x', 'o', 'o', 'o', 'o', 'x'],
+    ['x', 'o', 'o', 'o', 'o', 'x'],
+    ['x', 'o', 'o', 'o', 'o', 'x'],
+    ['x', 'o', 'o', 'o', 'o', 'x'],
+    ['x', 'x', 'x', 'x', 'x', 'x']
+]
+MAP_CONSTANTS = {
+    "x": "boundary",
+    "o": "open_space"
+}
+
+# constant commands
+# DESCRIPTION = "description"
+# EXAMINE = "examine"
+# SOLVED = False
+# UP = "up", "north"
+# DOWN = "down", "south"
+# LEFT = "left", "west"
+# RIGHT = "right", "east"
+
+# generate map
+def print_map():
+    (player_x, player_y) = player.location
+    sight = 3
+    for y in range(len(MAP)):
+        row = MAP[y]
+        map_row = []
+        for x in range(len(row)):
+            tile = row[x]
+            tile_coordinates = f"{y}-{x}"
+            zone = (x < player_x + sight and x > player_x - sight) and (y < player_y + sight and y > player_y - sight)
+            explored = tile_coordinates in player.explored
+
+            if explored or zone:
+                map_row.append(tile)
+                if not explored:
+                    player.explored.append(tile_coordinates)
+            else:
+                map_row.append("-")
+            
+
+        print(" ".join(map_row))
+
+    print("===========")
+
+print_map()
+player.location = [3, 2]
+print_map()
